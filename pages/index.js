e1923d39bfa200d3dff29f1109fd41861f572ba4
Warren Gates <warrenckhan@gmail.com>
2019-03-27 13:47:24 -0400

    add moia-now frontend files

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Template } from "../src/components/common";
+import Link from 'next/link';
+const Index = () => (
+  <Template className={"container mx-auto mt-5 h-100"}>
+    <h1 className="text-center">Welcome to Moia Now</h1>
+  </Template>
+);
+
+export default Index;
