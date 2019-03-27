b583ebfe5adfccedb18a0be3b71313c4eab64815
Warren Gates <warrenckhan@gmail.com>
2019-03-27 15:52:10 -0400

    converted /index.js to class component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,37 @@
-import { Template } from "../src/components/common";
-import Link from 'next/link';
-const Index = () => (
-  <Template className={"container mx-auto mt-5 h-100"}>
-    <h1 className="text-center">Welcome to Moia Now</h1>
-  </Template>
-);
 
-export default Index;
+// import Link from 'next/link';
+// const Index = () => (
+//   <Template className={"container mx-auto mt-5 h-100"}>
+//     <h1 className="text-center">Welcome to Moia Now</h1>
+//   </Template>
+// );
+
+import { Template } from "../src/components/common";
+
+import {Component}from 'react';
+import io from "socket.io-client";
+class Index extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      msg:''
+    }
+  }
+  componentDidMount(){
+    this.socket = io()
+    this.socket.on('now', data =>{
+      this.setState({
+        msg:data.message
+      })
+    })
+  }
+  render(){
+    return (
+      <Template className={"container mx-auto mt-5 h-100"}>
+        <h1 className="text-center">Welcome to Moia Now</h1>
+        {" "}
+      </Template>
+    );
+    }
+  }
+  export default Index;
